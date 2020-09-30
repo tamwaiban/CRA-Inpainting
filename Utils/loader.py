Fixed silly scaling error since converting from sigmoid to tanh. Still fixing FF

Utils/loader.py
@@ -42,7 +42,7 @@ class RandomMaskLoader:
         y = x * slope + (c1 * r0 - c0 * r1) / (c1 - c0)
         thickness = tf.math.ceil(w / 2)
         yy = (tf.reshape(tf.math.floor(y), [-1, 1]) + tf.reshape(tf.range(-thickness - 1, thickness + 2), [1, -1]))
-        xx = tf.repeat(x, yy.shape[1])  # So this isn't working anymore.
+        xx = tf.repeat(x, yy.shape[1])  # So this isn't working anymore, because shape is None when building ops.
         values = tf.reshape(RandomMaskLoader.trapez(yy, tf.reshape(y, [-1, 1]), w), [-1])
         yy = tf.reshape(yy, [-1])
         limits_y = tf.math.logical_and(yy >= 0, yy < shape)
@@ -122,7 +122,7 @@ class ImageDataLoader:
         self.opt = opt
         self.file_list = glob.glob(opt.__flist__ + "/*.jpg") + glob.glob(opt.__flist__ + "/*.png")
         self.file_list = tf.random.shuffle(self.file_list)
-        assert self.opt.__mask_type__ in ALL_MASK_TYPES, "Invalid Mask Type"
+        assert self.opt.__mask_type__ in ALL_MASK_TYPES, "Invalid Mask Type, Valid Types: " + ' '.join(ALL_MASK_TYPES)
         assert (len(self.file_list) != 0), "Invalid Data Path"
         if self.opt.__gen_masks__:
             if self.opt.__mask_type__ == "single_bbox":
@@ -192,7 +192,7 @@ class BalancedAudioLoader:
         self.opt = opt
         self.file_list = glob.glob(self.opt.__flist__ + "/*.mp3") + glob.glob(self.opt.__flist__ + "/*.wav")
         self.file_list = tf.random.shuffle(self.file_list)
-        assert self.opt.__mask_type__ in ALL_MASK_TYPES, "Invalid Mask Type"
+        assert self.opt.__mask_type__ in ALL_MASK_TYPES, "Invalid Mask Type, Valid Types: " + ' '.join(ALL_MASK_TYPES)
         assert (len(self.file_list) != 0), "Invalid Data Path"
         if self.opt.__gen_masks__:
             if self.opt.__mask_type__ == "single_bbox":

infer.py
@@ -29,11 +29,13 @@ if __name__ == "__main__":
         r_, g_, b_, mask = tf.split(input_, 4, axis=1)
         img = tf.concat((r_, g_, b_), axis=1)
         first_output, second_output = generator(input_, training=False)
-        fusion_fake1 = img * (1 - mask) + first_output * mask
-        fusion_fake2 = img * (1 - mask) + second_output * mask
-        masked_input = img * (1 - mask) + mask
+        masked = img * (1 - mask)
+        fusion_fake1 = masked + first_output * mask
+        fusion_fake2 = masked + second_output * mask
+        masked_input = masked + mask
+
         img_save = tf.concat((img, masked_input, first_output, second_output, fusion_fake1, fusion_fake2), axis=3)
-        img_save = tf.cast((img_save + 1) * 127.5, tf.uint8)
+        img_save = tf.cast(img_save * 255, tf.uint8)
         img_save = tf.transpose(img_save, perm=[0, 2, 3, 1])[0, :, :, :]
         jpeg_string = tf.io.encode_jpeg(img_save)
         tf.io.write_file("Examples/%s_output.jpg" % step, jpeg_string)

trainer.py
@@ -90,7 +90,7 @@ class WassersteinTrainer:
     def sample_model(self, img, mask, first_out, second_out, fout_whole, sout_whole, epoch, sample_folder):
         masked_img = img * (1 - mask) + mask
         img_save = tf.concat((img, masked_img, first_out, second_out, fout_whole, sout_whole), axis=3)
-        img_save = (img_save + 1) * 127.5
+        img_save = img_save * 255
         img_copy = tf.transpose(img_save, perm=[0, 2, 3, 1])[0, :, :, :]
         img_copy = tf.clip_by_value(img_copy, 0, 255)
         img_copy = tf.cast(img_copy, tf.uint8)